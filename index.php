fe0038cfe9b35b94723e54c954bdb47ee83d0acd
KresimirB <40023570+KresimirB@users.noreply.github.com>
2021-12-13 13:14:04 +0100

    Delete index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-//if($_SESSION["logged"]!==TRUE){
-//    header("Location: login");
-//}
-include('config/setup.php');
-include('template/header.php');
-include('template/body.php');
-include('template/footer.php');
