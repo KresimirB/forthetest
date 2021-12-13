969963fef9285287e094fa489bd8e4775704d469
KresimirB <40023570+KresimirB@users.noreply.github.com>
2021-12-13 12:03:44 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+//if($_SESSION["logged"]!==TRUE){
+//    header("Location: login");
+//}
+include('config/setup.php');
+include('template/header.php');
+include('template/body.php');
+include('template/footer.php');
